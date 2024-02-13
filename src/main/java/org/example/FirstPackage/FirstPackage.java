fa295eaf25449a5aa8424ca54cfa870ab97f9533
im. <83586607+exodie@users.noreply.github.com>
2024-02-13 10:23:25 +0400

    add labs-1

---
+++ src/main/java/org/example/FirstPackage/FirstPackage.java
@@ -0,0 +1,30 @@
+package org.example.FirstPackage;
+
+public class FirstPackage {
+    private int a1;
+    private int a2;
+
+    public int getA1() {
+        return a1;
+    }
+
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+
+    public int getA2() {
+        return a2;
+    }
+
+    public void setA2(int a2) {
+        this.a2 = a2;
+    }
+
+    public FirstPackage() {
+        System.out.println("FirstPackage constructor initialization success");
+    }
+
+    public int sum() {
+        return this.a1 + this.a2;
+    }
+}
--- src/main/java/org/example/FirstProgram.java
+++ src/main/java/org/example/FirstProgram.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.FirstPackage.*;
+
 class FirstProgram {
     public static void main(String[] s) {
-        System.out.println("Hello world!!!");
+        FirstPackage o = new FirstPackage();
+
+        int i, j;
+        for (i = 1; i <= 8; i++) {
+            for(j = 1; j <= 8; j++) {
+                o.setA1(i);
+                o.setA2(j);
+                System.out.print(o.sum());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
+
